2724bf8081406b9e9f3fbca4adc957bfd853d3fd
Alan <144842474+SeccreDev@users.noreply.github.com>
2025-03-27 02:16:56 -0400

    add player shape and rotation logic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -10,7 +11,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    delta_time = 0
     running = True
+
+    # Player Initialization
+    p1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while running:
         for event in pygame.event.get():
@@ -21,12 +26,14 @@ def main():
         screen.fill("black")
 
         # RENDER YOUR GAME HERE
-
+        p1.update(delta_time)
+        p1.draw(screen)
         # Flip the display to put your work on screen
         pygame.display.flip()
 
-        # Limit FPS to 60
-        clock.tick(60)
+        # Limit FPS to 60. delta_time is used for framerate-independent physics
+        delta_time = clock.tick(60) / 1000
+
     pygame.quit()
     
 if __name__ == "__main__":
